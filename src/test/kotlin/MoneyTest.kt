9316e021502e3c429b2dfbf595122c5e982a80f0
uta-mori <mori_yuta@tre-inc.com>
2020-03-05 17:42:16 +0900

    Dollarの副作用を無くした

--- src/test/kotlin/MoneyTest.kt
+++ src/test/kotlin/MoneyTest.kt
@@ -5,10 +5,10 @@ class MoneyTest {
     @Test
     fun testMultiplication() {
         val five = Dollar(5)
-        five.times(2)
-        assertEquals(10, five.amount)
-        five.times(3)
-        assertEquals(15, five.amount)
+        var product = five.times(2)
+        assertEquals(10, product.amount)
+        product = five.times(3)
+        assertEquals(15, product.amount)
     }
 }
 
